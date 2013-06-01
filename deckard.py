9b8cc1de0c55000923266d47750791775ef89a08
nateshortsleeve <nateshortsleeve@gmail.com>
2013-06-01 19:37:10 -0400

    targeting faces.

--- deckard.py
+++ deckard.py
@@ -1,32 +1,44 @@
 from SimpleCV import *
 from time import sleep
-
 import serial
-                
-def SearchForFace(frame):
-        #returns coordinates of face found as a tuple
-        #returns None if no face found
-        faces = frame.findHaarFeatures('face')
-        if faces:
-                for face in faces:
-                        #print "found face"
+
+class Deckard:
+    @staticmethod
+    def returnToCenter():
+        #for x in range(0, 180):
+        #    arduino.write('a')
+        #for x in range(0,90):
+        #    arduino.write('d')
+        for y in range(0, 180):
+            arduino.write('w')
+        #for y in range(0,90):
+        #    arduino.write('s')
+
+    
+
+if __name__ == '__main__':
+    arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    arduino.open()
+    Deckard.returnToCenter()
+
+    servo_position = (0,0)
+    increment = 1
+    w = 320
+    h = 240
+    center = (w/2, h/2)
+    camera = Camera(0)
+    display = Display(resolution=(w, h))
+    facebox_dim = (w/3,h/2)
+    focal_target = center
+    while not display.isDone():
+            frame = camera.getImage()
+            faces = frame.findHaarFeatures('face')
+            returnTuple = (-1,-1)
+            if faces:
+                    for face in faces:
                         print face.coordinates()
-                        return face.coordinates()
-        else:
-                #print "no faces found"
-                return None
-
-
-w = 320
-h = 240
-camera = Camera(0)
-display = Display(resolution=(w, h))
-facebox_dim = (w/3,h/2)
-coordinates = None
-while not display.isDone():
-        frame = camera.getImage()
-        coodinates = SearchForFace(frame)
-        if coordinates != None:
-                print coodinates[0], ' ', coordinates[1]
-        frame.save(display)
-exit()
+                        break
+            
+            frame.save(display)
+    arduino.close()
+    exit()
