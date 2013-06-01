3634302126ae02b946d63561c76929bb9407288f
nateshortsleeve <nateshortsleeve@gmail.com>
2013-06-01 18:48:40 -0400

    now reliably detecting faces

--- deckard.py
+++ deckard.py
@@ -2,19 +2,31 @@ from SimpleCV import *
 from time import sleep
 
 import serial
+                
+def SearchForFace(frame):
+        #returns coordinates of face found as a tuple
+        #returns None if no face found
+        faces = frame.findHaarFeatures('face')
+        if faces:
+                for face in faces:
+                        #print "found face"
+                        print face.coordinates()
+                        return face.coordinates()
+        else:
+                #print "no faces found"
+                return None
+
+
 w = 320
 h = 240
 camera = Camera(0)
 display = Display(resolution=(w, h))
 facebox_dim = (w/3,h/2)
+coordinates = None
 while not display.isDone():
         frame = camera.getImage()
-        faces = frame.findHaarFeatures('face')
-        if faces:
-                for face in faces:
-                        facelayer = DrawingLayer((frame.width, frame.height))
-                        facebox = facelayer.centeredRectangle(face.coordinates(), facebox_dim)
-                        frame.addDrawingLayer(facelayer)
-                        frame.applyLayers()
-                        break
+        coodinates = SearchForFace(frame)
+        if coordinates != None:
+                print coodinates[0], ' ', coordinates[1]
         frame.save(display)
+exit()
